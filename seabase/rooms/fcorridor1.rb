dc94664c8649a1348e6a9a666c2816f40cad741f
Lee-Jon <leejonball@gmail.com>
2013-03-06 23:29:29 +0000

    First floor Corridor2 added

--- seabase/rooms/fcorridor1.rb
+++ seabase/rooms/fcorridor1.rb
@@ -4,9 +4,8 @@ room(:fcorridor1) do
     entrance is south and a walkway also leads north.
   DESC
   
-  # Exits
-  
-  # Logic
-  
-  # Items
+  self.exit_south = :lift1b
+  self.exit_north = :bottle_store
+  self.exit_east  = :fcorridor2
+  self.exit_west  = :fcorridor4
 end
--- seabase/rooms/fcorridor2.rb
+++ seabase/rooms/fcorridor2.rb
@@ -1,10 +1,15 @@
 room(:fcorridor2) do
   self.short_desc = "fcorridor2"
   self.desc = <<-DESC
-    
+    As the corridor continues East/West, I begin to with I was still on the
+    sub'. At least the gangwaus were straight!
+    There are also compartments to the north and south.
   DESC
   
-  # Exits
+  self.exit_south = :
+  self.exit_north = :surgery
+  self.exit_east  = :fcorridor1
+  self.exit_west  = :fcorridor3
   
   # Logic
   
