6f7d640c64e83a8a2d6b3eacf5c9d70a7cffa3b8
Lee-Jon <leejonball@gmail.com>
2013-03-15 09:31:32 +0000

    Add kitchen

--- seabase/rooms/kitchen.rb
+++ seabase/rooms/kitchen.rb
@@ -1,12 +1,21 @@
 room(:kitchen) do
   self.short_desc = "kitchen"
   self.desc = <<-DESC
-    
+    I am in a well ised KITCHEN. Exit is WEST.
   DESC
   
-  # Exits
+  self.exit_west = :dinning_room
   
-  # Logic
-  
-  # Items
+  item(:shelves, 'shelves') do
+    self.presence = "Shelves"
+    self.script_examine = <<-SCRIPT
+      #examining shows bag of flour
+    SCRIPT
+  end
+  item(:hotplate, 'hotplate') do
+    self.presence = "Hotplate"
+  end
+  item(:fridge, 'fridge') do
+    self.presence = "Fridge"
+  end
 end
